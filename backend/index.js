0653c590362e4f4f8fa268f49f5d237244f39f5f
William Ramirez <86168463+WARFiuba@users.noreply.github.com>
2024-06-27 14:23:05 -0300

    cambios

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,10 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const rootDir = process.cwd();
-    const pathFile = path.join(rootDir, "frontend", "index.html");
-
-    const html = fs.readFileSync(pathFile);
-
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
-    res.end("<h1>hola node.js</h1><p>lorem</p>");
-});
+app.get('/', (req, res) => {
+    res.send("hola express")
+})
 
 const PORT = 3000;
 
-server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
