ce9bdec37cfe9a4dd4f360ff9179ef60beba1eb4
William Ramirez <86168463+WARFiuba@users.noreply.github.com>
2024-06-29 16:41:26 -0300

    uso de env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
 import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config({path: './.env/.env'});
 
 const pool = createPool({
-    host: 'mysql-warfiuba.alwaysdata.net',
-    user: 'warfiuba',
-    password: '29jun24qwe+',
-    database: 'warfiuba_cac',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit: 5
 });
 
